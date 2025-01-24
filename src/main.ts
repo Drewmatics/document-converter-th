a4350d2294d16277c4e8881ea72679236bbf0e4a
Drew Leung <leungdrew@gmail.com>
2025-01-23 18:38:40 -0800

    Adding a comment

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  //set HTTP headers for security, XSS prevention
   app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
