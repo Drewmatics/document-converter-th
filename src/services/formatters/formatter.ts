a9ed1c07dbff0b3d7469ba706c286ca700b23654
Drew Leung <leungdrew@gmail.com>
2025-01-23 22:12:38 -0800

    Forgot to remove nullable operator

--- src/services/formatters/formatter.ts
+++ src/services/formatters/formatter.ts
@@ -2,5 +2,5 @@ import { InputData } from "src/types/InputData";
 import { ParseDocumentParams } from "src/types/ParseDocumentParams";
 
 export interface Formatter {
-  format(inputData: InputData, params?: ParseDocumentParams): string;
+  format(inputData: InputData, params: ParseDocumentParams): string;
 }
